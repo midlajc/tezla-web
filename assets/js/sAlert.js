1fb5419bdb5584928bf0eb817f8398498abd33a0
midlajc <mail.midlajc@gmail.com>
2022-03-23 17:31:18 +0530

    link issue fixed

--- assets/js/sAlert.js
+++ assets/js/sAlert.js
@@ -362,7 +362,7 @@ const fireFindMeOut = () => {
                 </tr>
             </table>
             <div style="margin:0.5rem">
-            <a class="btn btn-primary" href="https://forms.gle/dZ7seyu5dVCg4n2S9" target="_blank">Submit Your Answer</a>
+            <a class="btn btn-primary" href="https://forms.gle/M5874ME35LmFyDh37" target="_blank">Submit Your Answer</a>
             </div>
             </div>
             `,
